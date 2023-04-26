49ce0db1ca3c111333690cea181d6423e0a17eb7
denizcevik <denizcev@gmail.com>
2023-04-26 10:46:00 +0300

    Add files via upload

---
+++ flask_token_verify.py
@@ -0,0 +1,15 @@
+from flask_unsign import session
+session_cookie="eyJfdXNlcl9pZCI6MSwidXNlcl9pZCI6MX0.ZEfQPg.Enz83rUqMAFfdCds7ClQzlEmScg";
+session.decode(session_cookie)
+session.verify(session_cookie, '\x02\x01thisismyscretkey\x01\x02\\e\\y\\y\\h')
+
+#### DEFAULT KEYS ####
+
+# CHANGE_ME_TO_A_COMPLEX_RANDOM_SECRET
+# thisISaSECRET_1234
+# YOUR_OWN_RANDOM_GENERATED_SECRET_KEY
+# TEST_NON_DEV_SECRET
+
+
+# More complete word list
+# https://github.com/Paradoxis/Flask-Unsign-Wordlist
